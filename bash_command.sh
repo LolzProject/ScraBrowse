ffe792023f0be36fea050129722ae7dc6b69f687
AwesomePotatoCodes <105654727+AwesomePotatoCodes@users.noreply.github.com>
2022-05-16 13:18:10 -0500

    Update bash_command.sh

--- bash_command.sh
+++ bash_command.sh
@@ -4,3 +4,7 @@ python3 -V # check your python version
 # if it's higher than 3.8 you're good
 python3 main.py
 # this runs the file, have fun
+# i use vscode so i don't really
+# use my shell / command line
+
+echo Goodbye!
