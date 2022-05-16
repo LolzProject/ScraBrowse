be5b8ce2b974eb77d2734a3141bd1f006f48d440
AwesomePotatoCodes <105654727+AwesomePotatoCodes@users.noreply.github.com>
2022-05-16 13:17:24 -0500

    Create bash_command.sh

---
+++ bash_command.sh
@@ -0,0 +1,6 @@
+# if you downloaded the file and plan on using it in your terminal, here's the script to run it.
+
+python3 -V # check your python version
+# if it's higher than 3.8 you're good
+python3 main.py
+# this runs the file, have fun
